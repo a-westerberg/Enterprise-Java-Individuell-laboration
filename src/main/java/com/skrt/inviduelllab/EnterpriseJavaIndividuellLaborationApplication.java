b43e4131db356902ba1e8176bf33bfecc13733c7
aaalx <alexander.westerberg.svedberg@gmail.com>
2025-04-01 13:43:24 +0200

    first commit

---
+++ src/main/java/com/skrt/inviduelllab/EnterpriseJavaIndividuellLaborationApplication.java
@@ -0,0 +1,13 @@
+package com.skrt.inviduelllab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseJavaIndividuellLaborationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseJavaIndividuellLaborationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skrt/inviduelllab/EnterpriseJavaIndividuellLaborationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skrt.inviduelllab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseJavaIndividuellLaborationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
